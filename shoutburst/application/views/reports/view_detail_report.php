932cd798fb98a465b4a079e4ffdd443f268d7cd1
DunAlex <aleksandun@gmail.com>
2015-03-31 16:44:03 +0300

    recordings

--- shoutburst/application/views/reports/view_detail_report.php
+++ shoutburst/application/views/reports/view_detail_report.php
@@ -1,10 +1,7 @@
 <?php
-if(!empty($report)){
-
-$is_dash = false;
-if ($this->uri->segment(4) == 'd') {
-$is_dash = true;
-}
+if(!empty($report))
+{
+	$is_dash = $this->uri->segment(4) == 'd';
 
 	$query					=	$report['report_query'];
 	$report_type			=	$report['report_type'];
@@ -17,36 +14,36 @@ $is_dash = true;
 	$end_date				=	$report['custom_end_date'];
 	
 	$requestedFromList		=	'Request From List';
+
 	?>
 	<div id="content">
 		<div class="container">	
-<?php
-if (!$is_dash) {
-?>
-
-			<div class="row content-header">
-				<h1>Detail Report View</h1>
-			</div>
-
-<?php
-}
-?>
-			<div class="row content-body">
-<?php
-if (!$is_dash) {
-?>
-				
-				<div style='float:right; padding-bottom:10px; padding-right:10px;'>
-				  <!--	<a href='javascript:;' onClick='return false;' class='btn btn-primary'>Export to PDF</a>-->
-					<a href='<?php echo base_url().'reports'?>' class='btn btn-primary'>Back</a>
-					<a href='<?php echo base_url();?>reports/print_csv/<?php echo $report_id;?>'  class='btn btn-primary'>Export to CSV</a>					
-				</div>
-
-<?php
-}
-?>
-
-
+	<?php
+	if (!$is_dash)
+	{
+	?>
+		<div class="row content-header">
+			<h1>Detail Report View</h1>
+		</div>
+	<?php
+	}
+	?>
+		<div class="row content-body">
+	<?php
+	if (!$is_dash)
+	{
+	?>
+		<div style='float:right; padding-bottom:10px; padding-right:10px;'>
+		  <!--	<a href='javascript:;' onClick='return false;' class='btn btn-primary'>Export to PDF</a>-->
+			<a href='<?php echo base_url().'reports'?>' class='btn btn-primary'>Back</a>
+			<a href='<?php echo base_url();?>reports/print_csv/<?php echo $report_id;?>'  class='btn btn-primary'>Export to CSV</a>
+		</div>
+	<?php
+	}
+	?>
+			<audio id="audio" style="display: none;">
+				<source src="">
+			</audio>
 				<div class="col-sm-12">
 					<div id="report_content">
 						<div class='queryBuilderHtml'>
@@ -61,4 +58,3 @@ if (!$is_dash) {
 	</div>
  	<?php 
 }
-?>
